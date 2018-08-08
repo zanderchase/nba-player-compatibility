9273a2da2dd4d3562d2c20a1b868cd03f9292ae8
zanderchase <zanderchase@gmail.com>
2018-08-07 18:06:26 -0700

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -6,7 +6,7 @@ dropdown2.length = 0;
 let defaultOption = document.createElement('option');
 defaultOption.text = 'Choose Player';
 
-let defaultOption2 = document.createElement('option2');
+let defaultOption2 = document.createElement('option');
 defaultOption2.text = 'Choose Player';
 
 dropdown.add(defaultOption);
@@ -23,18 +23,12 @@ request.onload = function() {
   if (request.status === 200) {
     const data = JSON.parse(request.responseText);
     let option;
-    let option2;
     for (let i = 0; i < data.length; i++) {
       option = document.createElement('option');
       option.text = data[i].name;
       option.value = data[i].id;
       dropdown.add(option);
       dropdown2.add(option);
-      option2 = document.createElement('option2');
-      option2.text = data[i].name;
-      option2.value = data[i].id;
-      dropdown.add(option2);
-      dropdown2.add(option2);
     }
    } else {
     // Reached the server, but it returned an error
