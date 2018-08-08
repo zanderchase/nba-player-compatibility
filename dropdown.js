37c4a55e04e9a9e6c765885b9e9740797a1885e4
zanderchase <zanderchase@gmail.com>
2018-08-07 17:57:43 -0700

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -6,9 +6,12 @@ dropdown2.length = 0;
 let defaultOption = document.createElement('option');
 defaultOption.text = 'Choose Player';
 
+let defaultOption2 = document.createElement('option2');
+defaultOption2.text = 'Choose Player';
+
 dropdown.add(defaultOption);
 dropdown.selectedIndex = 0;
-dropdown2.add(defaultOption);
+dropdown2.add(defaultOption2);
 dropdown2.selectedIndex = 0;
 
 const url = 'player_info.json';
@@ -20,6 +23,7 @@ request.onload = function() {
   if (request.status === 200) {
     const data = JSON.parse(request.responseText);
     let option;
+    let option2;
     for (let i = 0; i < data.length; i++) {
       option = document.createElement('option');
       option.text = data[i].name;
@@ -27,6 +31,12 @@ request.onload = function() {
       option.onchange = action_function();
       dropdown.add(option);
       dropdown2.add(option);
+      option2 = document.createElement('option2');
+      option2.text = data[i].name;
+      option2.value = data[i].id;
+      option2.onchange = action_function();
+      dropdown.add(option2);
+      dropdown2.add(option2);
     }
    } else {
     // Reached the server, but it returned an error
