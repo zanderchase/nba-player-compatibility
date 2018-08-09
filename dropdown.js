35de1025e0869e82c0d839d9221f83bc8962187a
zanderchase <zanderchase@gmail.com>
2018-08-09 15:16:44 -0700

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,17 +1,4 @@
-//let dropdown = document.getElementById('locality-dropdown');
 let dropdown = document.getElementById('myDropdown');
-//dropdown.length = 0;
-
-//let defaultOption = document.createElement('option');
-//defaultOption.text = 'Choose Player 1';
-
-//let defaultOption2 = document.createElement('option');
-//defaultOption2.text = 'Choose Player 2';
-
-//dropdown.add(defaultOption);
-//dropdown.selectedIndex = 0;
-//dropdown2.add(defaultOption2);
-//dropdown2.selectedIndex = 0;
 
 const url = 'player_info.json';
 
@@ -27,7 +14,6 @@ request.onload = function() {
       a.text = data[i].name;
       a.value = data[i].id;
       dropdown.add(a);
-      //dropdown2.add(option);
     }
    } else {
     // Reached the server, but it returned an error
