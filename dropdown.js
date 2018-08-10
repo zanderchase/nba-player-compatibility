35fd8e1813dc7ef6ee690a0887b24c14665079b4
zanderchase <zanderchase@gmail.com>
2018-08-09 18:18:22 -0700

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,16 +1,17 @@
-//let dropdown = document.getElementById('locality-dropdown');
-//dropdown.length = 0;
+let dropdown = document.getElementById('locality-dropdown');
+dropdown.length = 0;
+let dropdown2 = document.getElementById('locality-dropdown2');
+dropdown2.length = 0;
 
-//let defaultOption = document.createElement('option');
-//defaultOption.text = 'Choose Player 1';
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Choose Player 1';
+let defaultOption2 = document.createElement('option');
+defaultOption2.text = 'Choose Player 2';
 
-//let defaultOption2 = document.createElement('option');
-//defaultOption2.text = 'Choose Player 2';
-
-//dropdown.add(defaultOption);
-//dropdown.selectedIndex = 0;
-//dropdown2.add(defaultOption2);
-//dropdown2.selectedIndex = 0;
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+dropdown2.add(defaultOption2);
+dropdown2.selectedIndex = 0;
 
 const url = 'player_info.json';
 
@@ -21,13 +22,18 @@ request.onload = function() {
   if (request.status === 200) {
     const data = JSON.parse(request.responseText);
     var currentDiv = document.getElementById("myInput");
-    console.log(data.length);
+    let option
+    let option2
     for (let i = 0; i < data.length; i++) {
-      console.log(data[i].name);
-      newa = document.createElement('a');
-      newa.text = data[i].name;
-      newa.value = data[i].id;
-      currentDiv.appendChild(newa);
+      option = document.createElement('option');
+      option.text = data[i].name;
+      option.value = data[i].id;
+      dropdown.add(option)
+      
+      option2 = document.createElement('option');
+      option2.text = data[i].name;
+      option2.value = data[i].id;
+      dropdown2.add(option2)
     }
    } else {
      console.log('fail');
@@ -41,29 +47,10 @@ request.onerror = function() {
 
 request.send();
 
-action_function = function() {
-    var newaction = this.value;
-    console.log(newaction);
-};
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+document.getElementById("locality-dropdown").onchange = function(){
+  console.log(this.value);
 }
 
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
-  }
+document.getElementById("locality-dropdown2").onchange = function(){
+  console.log(this.value);
 }
-
